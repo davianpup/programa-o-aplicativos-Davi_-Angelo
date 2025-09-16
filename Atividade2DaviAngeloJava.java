6cb15817415fdd3689d58faf5c271659478b3f6a
davianpup <davianp651@gmail.com>
2025-09-16 20:47:14 -0300

    Add files via upload

---
+++ Atividade2DaviAngeloJava.java
@@ -0,0 +1,176 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade2.davi.angelo.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade2DaviAngeloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    // Questão 1
+    public static void main(String[] args) {
+      // Scanner entrada = new Scanner(System.in);
+      // double raio,area,circunf;
+       
+      // System.out.print("Digite o valor do raio do circulo:");
+      // raio = entrada.nextDouble();
+       //area = Math.PI*(Math.pow(raio,2));
+       //circunf = 2*Math.PI*raio;
+       
+      // System.out.println("Resultado área:"+area);
+      // System.out.println("Resultado Circunferência:"+circunf);
+       
+       //Questão 2
+       //Scanner input = new Scanner(System.in);
+       //double peso,altura,imc;
+       
+       //System.out.print("Digite o peso: ");
+       //peso = input.nextDouble();
+       //System.out.print("Digite a altura: ");
+       //altura = input.nextDouble();
+       
+       //imc = peso / (altura*altura);
+       //System.out.print("O IMC é: "+imc);
+       
+       //Questão 3
+       //Scanner input = new Scanner(System.in);
+       //int cat1,cat2;
+      //double hipot;
+       
+       //System.out.print("Dê o comprimento do cateto 1:");
+      // cat1 = input.nextInt();
+       
+      //System.out.print("Dê o comprimento do cateto 2:");
+       //cat2= input.nextInt();
+       
+       //hipot = (cat1*cat1) + (cat2*cat2);
+       
+       //System.out.print("A hipotenusa é: "+hipot);
+       //Questão 4
+       //Scanner input = new Scanner(System,in);
+       //double real,c_d,d_g,v_g,troco;
+       
+       //System.out.print("Dê o valor em reais: ");
+       //real = input.nextInt();
+       
+       //System.out.print("Dê a cotação do dolar: ");
+       //c_d = input.nextInt();
+       
+       //d_g = real/c_d;
+       
+       //v_g = d_g*c_d;
+
+       //troco = real - (d_g*c_d);
+       //System.out.print("Troco em reais: "+v_g);
+       
+       //Questão 5
+       //int n,j1,j2,s;
+       //Scanner input = new Scanner(System.in);
+       //System.out.print("Insira a quantidade de jogadas: ");
+       //n = input.nextInt();
+       
+       //System.out.print("Insira o 1° valor: ");
+       //j1 = input.nextInt();
+       
+       //System.out.print("Insira o 2° valor: ");
+       //j2 = input.nextInt();
+       
+       //s = (j1 + j2)/n;
+       //System.out.print("A média é:"+s );
+       
+       //System.out.print("N° maior é: "+Math.max(j1,j2));
+       //System.out.print("N° menor é: "+Math.min(j1,j2));
+      
+       //Questão 6
+       //Scanner input = new Scanner(System.in);
+       //double angulo,vsen,vcos,vtan,rad;
+       
+       //System.out.print("Dê um Ângulo: ");
+       //angulo = input.nextInt();
+       
+       //rad = Math.toRadians(angulo);
+       //vtan = Math.tan(rad);
+       //vcos = Math.cos(rad);
+       //vsen = Math.sin(rad);
+       
+       
+       //System.out.print("Valor do Seno: "+vtan);
+       //System.out.print("Valor do Cosseno: "+vcos);
+       //System.out.print("Valor do Tangente: "+vsen);
+       
+       //Questão 7
+       
+       //double x1,y1,x2,y2,calc;
+       //Scanner input = new Scanner(System.in);
+       
+       //System.out.print("Digite a coordenada x1:");
+       //x1 = input.nextDouble();
+       
+       //System.out.print("Digite a coordenada x1");
+       //y1 = input.nextDouble();
+       
+       //System.out.print("Digite a coordenada x1");
+       //x2 = input.nextDouble();
+       
+       //System.out.print("Digite a coordenada x1");
+       //y2 = input.nextDouble();
+       
+       //calc = Math.sqrt(Math.pow((x2 - x1),2)) + (Math.pow((y2 - y1),2);
+       
+       //Questão 8
+       //Scanner input = new Scanner(System.in);
+       //double n1,n2,n3,media,soma,dif;
+       
+       //n1 = input.nextDouble();
+       //n2 = input.nextDouble();
+       //n3 = input.nextDouble();
+       //media = (n1+n2+n3)/3;
+       
+       //soma = Math.abs(n1) + Math.abs(n2) + Math.abs(n3);
+       
+       //dif = Math.abs(n1-n3);
+       
+       //Questão 9
+       //Scanner input = new Scanner(System.in);
+       //int a,b,c;
+       //double x1,x2,delta;
+       
+       //a = input.nextInt();
+       //b = input.nextInt();
+       //c = input.nextInt();
+       
+       //delta = Math.sqrt((Math.pow(b,2))-(4*a*c));
+       //x1 = ((0 - b ) + delta)/(2*a);
+       //x2 = ((0 - b ) + delta)/(2*a);
+       
+       //System.out.print("O valor de x1 é: "+x1);
+       //System.out.print("O valor de x2 é: "+x2);
+       
+       //Questão 10
+       
+       //Scanner input = new Scanner(System.in);
+       //double vFh,vC;
+       
+       //System.out.print("Dê a temperatura em Fahrenheit: ");
+       //vFh = input.nextDouble();
+       
+       //vC = vFh-32/1.8;
+       
+       //System.out.print("Valor em Fahrenheit: "+vC);
+       
+       
+       
+       
+       
+       
+       
+    }
+    
+}
