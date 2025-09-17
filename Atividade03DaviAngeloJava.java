9b4200b64b784565b4b88fce4ce3628352f16a3f
davianpup <davianp651@gmail.com>
2025-09-17 20:24:24 -0300

    Add files via upload

---
+++ Atividade03DaviAngeloJava.java
@@ -0,0 +1,261 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03.davi.angelo.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade03DaviAngeloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Questão 1
+        //Scanner input = new Scanner(System.in);
+        //int entrada,qtd;
+        
+        //System.out.print("Digite a quantidade de porções: ");
+        //entrada = input.nextInt();
+        
+        //qtd = entrada*25;
+        
+        //System.out.print("Quantidade de açucar necessário(g): "+qtd);
+        
+        //Questão 2
+        //Scanner input = new Scanner(System.in);
+        //int imp,min;
+        
+        //System.out.print("Digite a quantidade de impressoras: ");
+        //imp = input.nextInt();
+        
+        //min = (imp*12) / 3;
+        //System.out.print("As impressoras imprimem em: ");
+        
+        //Questão 3
+        //Scanner input = new Scanner(System.in);
+        //double km,L;
+        
+        //System.out.print("Digite a distância em km: ");
+        //km = input.nextDouble();
+        
+        //L = (km*18) / 240;
+        
+        //System.out.print("Quantidade de litros necessários: "+L);
+        
+        //Questão 4
+//        int maq;
+//        double temp,form,pec;
+//        Scanner input = new Scanner(System.in);
+//        
+//        System.out.print("Digite a quantidade de maquinas: ");
+//        maq = input.nextInt();
+//        
+//        System.out.print("Digite o tempo de produção: ");
+//        temp = input.nextDouble();
+//        
+//        form = (6 / maq) * (5 / temp);
+//        
+//        pec = 900 / form;
+//        
+//        System.out.print("Quantidade de peças produzidas: "+pec);
+//        System.out.print("Tempo de produção: "+temp);
+//        System.out.print("Quantidade de Maquinas utilizadas: : "+maq);
+//        
+        //Questão 5
+//        double vR,cD,vD;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite a cotação do dolar: ");
+//        cD = input.nextDouble();
+//        
+//        System.out.print("Digite o valor em dolar: ");
+//        vD = input.nextDouble();
+//        
+//        vR = cD * vD;
+//        
+//        System.out.print("Valor em reais: "+vR);
+//        
+          //Questão 6
+//          double a,b,x,equacao;
+//          Scanner input = new Scanner(System.in);
+//          System.out.print("Digite o valor de a: ");
+//          a = input.nextDouble();
+//          
+//          System.out.print("Digite o valor de b: ");
+//          b = input.nextDouble();
+//        
+//          x = -b/a;
+//          equacao = (a*x) + b;
+//          
+//          System.out.print("Valor de a: "+a);
+//          System.out.print("Valor de b: "+b);
+//          System.out.print("Valor de x: "+x);
+
+            //Questão 7
+//            double m,b,x0,y0;
+//            Scanner input = new Scanner(System.in);
+//            
+//            System.out.print("Digite o valor de m: ");
+//            m = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de b: ");
+//            b = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de x0: ");
+//            x0 = input.nextDouble();
+//            
+//            y0 = m*x0+b;
+//            System.out.print("Valor de y0: "+y0);
+              
+            //Questão 8
+//            double x1,x2,y1,y2,m,b,y;
+//            Scanner input = new Scanner(System.in);
+//            System.out.print("Digite o valor de x1: ");
+//            x1 = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de x1: ");
+//            y1 = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de x1: ");
+//            x2 = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de x1: ");
+//            y2 = input.nextDouble();
+//            
+//            m = (y2-y1)/(x2-x1);
+//            b = y1-(m*x1);
+//            y = (m*x1)+b;
+//            
+//            System.out.print("Resultado da formula é: "+y);
+            
+            //Questão 9
+//            double m1,b1,m2,b2,x,y;
+//            Scanner input = new Scanner(System.in);
+//            System.out.print("Digite o valode de m1: ");
+//            m1 = input.nextDouble();
+//            
+//            System.out.print("Digite o valode de m2: ");
+//            m2 = input.nextDouble();
+//            
+//            System.out.print("Digite o valode de b1: ");
+//            b1 = input.nextDouble();
+//            
+//            System.out.print("Digite o valode de b2: ");
+//            b2 = input.nextDouble();
+//            
+//            x = (b2-b1)/(m1-m2);
+//            y = (m1*x)+b1;
+//            
+//            System.out.print(x+","+y);
+             
+            //Questão 10
+//            double a,b,c,D,x1,x2;
+//            Scanner input = new Scanner(System.in);
+//            System.out.print("Dê o valor de a:");
+//            a = input.nextDouble();
+//            
+//            System.out.print("Dê o valor de b:");
+//            b = input.nextDouble();
+//            
+//            System.out.print("Dê o valor de c:");
+//            c = input.nextDouble();
+//            
+//            D = Math.pow(b,2)-(4*a*c);
+//            x1 = (-b + Math.sqrt(D)) / (2*a);
+//            x2 = (-b + Math.sqrt(D)) / (2*a);
+//            
+//            System.out.print(D+","+x1+","+x2);
+
+            //Questão 11
+//            double a,b,c,D,dtc;
+//            Scanner input = new Scanner(System.in);
+//            System.out.print("Digite o valor de a: ");
+//            a = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de b: ");
+//            b = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de c: ");
+//            c = input.nextDouble();
+//            
+//            D = Math.pow(b,2)-(4*a*c);
+//            dtc = Math.abs((Math.sqrt(D))/a);
+//            
+//            System.out.print(d);
+
+            //Questão 12
+//            double a,b,c,x0,f1,f2;
+//            Scanner input = new Scanner(System.in);            
+//            System.out.print("Digite o valor de a: ");
+//            a = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de b: ");
+//            b = input.nextDouble();
+//           
+//            System.out.print("Digite o valor de c: ");
+//            c = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de c: ");
+//            x0 = input.nextDouble();
+//            
+//            f1 = (a*(Math.pow(x0,2)))+(b*x0)+c;
+//            f2 = (2*a*x0)+b;
+//            
+//            System.out.print("f(x0) = "+f1);
+//            System.out.print("f´(x0) = "+f2);
+       
+            //Questão 13
+//            double y2,m,b,x1,x2,y1,A;
+//            Scanner input = new Scanner(System.in);
+//            System.out.print("Digite o valor de m: ");
+//            m = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de b: ");
+//            b = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de x1: ");
+//            x1 = input.nextDouble();
+//            
+//            System.out.print("Digite o valor de x2: ");
+//            x2 = input.nextDouble();
+//            
+//            y1 = (m*x1)+b;
+//            y2 = (m*x2)+b;
+//            
+//            A = ((y1+y2)/2)*(x2-x1);
+//            
+//            System.out.print("Solução de A: "+A);
+//          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+        
+    }
+    
+}
