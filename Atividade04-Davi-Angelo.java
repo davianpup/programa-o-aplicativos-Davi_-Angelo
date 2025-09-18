53650979f20298b701d4a4759c9928a76bae5f25
davianpup <davianp651@gmail.com>
2025-09-18 20:01:00 -0300

    Add files via upload

---
+++ Atividade04-Davi-Angelo.java
@@ -0,0 +1,311 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade04.davi.angelo.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04DaviAngeloJava {
+
+    public static void main(String[] args) {
+//        int num;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite um número: ");
+//        num = input.nextInt();
+//        
+//        if (num < 0){
+//            System.out.print("Número negativo");
+//        }else{
+//            System.out.print("Número positivo");
+//        }
+        //Questão 2
+//        int num;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite um valor: ");
+//        num = input.nextInt();
+//        
+//        if(num<0){
+//            System.out.print("Número negativo");    
+//        }
+//        if(num>0){
+//            System.out.print("Número positivo");    
+//        }
+//        if(num==0){
+//            System.out.print("Número Zero");    
+//        }
+        
+        //Questão 3
+//        double nt1,nt2,nt3,media;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite a 1° nota: ");
+//        nt1 = input.nextDouble();
+//        
+//        System.out.print("Digite a 2° nota: ");
+//        nt2 = input.nextDouble();
+//        
+//        System.out.print("Digite a 3° nota: ");
+//        nt3 = input.nextDouble();
+//        
+//        media = nt1+nt2+nt3/3;
+//        if(media>=7){
+//            System.out.print("Aluno Aprovado");
+//        }else{
+//            System.out.print("Aluno Reprovado");
+//        }
+                
+        //Questão 4
+//        double nt1,nt2,nt3,media;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite a 1° nota: ");
+//        nt1 = input.nextDouble();
+//        
+//        System.out.print("Digite a 2° nota: ");
+//        nt2 = input.nextDouble();
+//        
+//        System.out.print("Digite a 3° nota: ");
+//        nt3 = input.nextDouble();
+//        
+//        media = nt1+nt2+nt3/3;
+//        if(media>=7){
+//            System.out.print("Aluno Aprovado");
+//        }
+//        if(media >=5 && media<7.0){
+//            System.out.print("Aluno de Recuperação");
+//        }
+//        if(media<5){
+//            System.out.print("Aluno Reprovado");
+//        }
+        
+        //Questão 5
+//        int num;
+//        Scanner input = new Scanner(System.in);
+//        num = input.nextInt();
+//        
+//        if(num %2==0){
+//            System.out.print("Número Par");
+//        }else{
+//            System.out.print("Número ímpar");
+//        }
+        
+       //Questão 6
+//       int num1,num2,num3,maior;
+//       Scanner input = new Scanner(System.in);
+//       System.out.print("Digite o 1° valor");
+//       num1 = input.nextInt();
+//       
+//       System.out.print("Digite o 2° valor");
+//       num2 = input.nextInt();
+//       
+//       System.out.print("Digite o 3° valor");
+//       num3 = input.nextInt();
+//        
+//       if(num1>num2 && num1>num3){
+//           System.out.print("Maior: "+num1);
+//       }
+//       if(num2>num1 && num2>num3){
+//           System.out.print("Maior: "+num2);
+//       }
+//        if(num3>num1 && num3>num2){
+//            System.out.print("Maior: "+num3);
+//        }
+        
+        //Questão 7
+//        int idd;
+//        Scanner input = new Scanner(System.in);
+//        idd = input.nextInt();
+//        
+//        if(idd>=18){
+//            System.out.print("Categoria Adulta");
+//        }
+//        if(idd>=14){
+//            System.out.print("Categoria Juvenil");
+//        }
+//        if(idd>=9){
+//            System.out.print("Categoria Infantil");
+//        }
+//        if(idd<9){
+//            System.out.print("Categoria Jucenil");
+//        }
+        
+        //Questão 8
+//        int num1,num2,soma;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite o 1° valor: ");
+//        num1 = input.nextInt();
+//        
+//        System.out.print("Digite o 2° valor: ");
+//        num2 = input.nextInt();
+//        
+//        soma = num1+num2;
+//        
+//        if(soma>20){
+//            System.out.print(soma+8);
+//        }else{
+//            System.out.print(soma-5);
+//        }
+        
+        //Questão 9
+//        double compra,venda1,venda2,porc1,porc2;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite o valor da compra: ");
+//        compra = input.nextDouble();
+//        
+//        if(compra<20){
+//            porc1 = (compra*45) / 100;
+//            venda1 = porc1 + compra;
+//            System.out.print("Valor vendido: "+venda1);
+//        }else{
+//            porc2 = (compra*30) / 100;
+//            venda2 = porc2 + compra;
+//            System.out.print("Valor vendido: "+venda2);
+//        }
+
+        //Questão 10
+//        double qtd,vlr1,vlr2;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite o quantidade comprada: ");
+//        qtd = input.nextDouble();
+//        
+//        if(qtd<12){
+//            vlr1 = qtd*0.30;
+//            System.out.print("Valor gasto: "+vlr1);
+//        }else{
+//            vlr2 = qtd*0.25;
+//            System.out.print("Valor gasto: "+vlr2);
+//        }
+
+        //Questão 11
+//        double altura,calt1,calt2;
+//        int sex;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite sua alura: ");
+//        altura = input.nextDouble();
+//        
+//        System.out.print("Digite seu sexo(1-H,2-F): ");
+//        sex = input.nextInt();
+//        
+//        switch (sex){
+//            case 1: calt1 = (72.7*altura)-58;
+//            System.out.print("Peso ideal: "+calt1);
+//            break;
+//            case 2: calt2 = (62.1*altura)-44.7;
+//            System.out.print("Peso ideal: "+calt2);
+//        }
+
+        //Questão 12
+        // int dS;
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Digite um dia da semana em n° int:  ")
+        // dS = input.nextInt();
+
+        // if(dS<5 && dS!=1){
+        //     System.out.print("Dia útil")
+        // }
+        // if(dS==7 || dS==1){
+        //     System.out.print("Final de semana")
+        // }else{
+        //     System.out.print("Dia inválido")
+        // }
+
+        //Questão 13
+        // Scanner input = new Scanner(System.in);
+        // double soma;
+        // int cod1,cod2,cod3;
+
+        // System.out.print("Digite o código do 1º produto: ");
+        // cod1 = input.nextInt();
+
+        // if(cod1 == 100) {
+        //     soma = soma+1.50;
+        // } 
+        // if(cod1 == 101) {
+        //     soma = soma+4.95;
+        // }
+        // if(cod1 == 102) {
+        //     soma = soma+1.79;
+        // }else {
+        //     System.out.println("Código inválido");
+        // }
+
+        // System.out.print("Digite o código do 2º produto: ");
+        // cod2 = input.nextInt();
+
+        // if(cod2 == 100) {
+        //     soma = soma+1.50;
+        // }
+        // if(cod2 == 101) {
+        //     soma = soma+4.95;
+        // }
+        // if(cod2 == 102) {
+        //     soma = soma+1.79;
+        // }else {
+        //     System.out.println("Código inválido");
+        // }
+
+        // System.out.print("Digite o código do 3º produto: ");
+        // cod3 = input.nextInt();
+
+        // if(cod3 == 100) {
+        //     soma = soma+1.50;
+        // }
+        // if(cod3 == 101) {
+        //     soma = soma+4.95;
+        // }
+        // if (cod3 == 102) {
+        //     soma = soma+1.79;
+        // }else {
+        //     System.out.println("Código inválido");
+        // }
+
+        // System.out.println("Valor total da compra: R$ " +soma);
+
+        //Questão 14
+        // doule soma,sub,mult,div,num1,num2;
+        // int opc;
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Escolha uma opção")
+        // System.out.print("1- Soma;")
+        // System.out.print("2- Subtração;")
+        // System.out.print("3- Multiplicação;")
+        // System.out.print("4- Divisão.")
+        // opc = input.nextInt();
+        // System.out.print("Digite o 1° número: ")
+        // num1 = input.nextDouble();
+
+        // switch (opc) {
+        //     case 1: soma = num1+num2;
+        //         System.out.print(num1+"+"+num2+"="+soma);
+        //         break;
+
+        //     case 2: sub = num1-num2;
+        //         System.out.print(num1+"-"+num2+"="+sub);
+        //         break;
+
+        //     case 3: mult = num1*num2;
+        //         System.out.print(num1+"*"+num2+"="+mult);
+        //         break;
+
+        //     case 3: div = num1/num2;
+        //         System.out.print(num1+"/"+num2+"="+div);
+        //         break;
+                
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
