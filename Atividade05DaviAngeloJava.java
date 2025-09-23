5457527db5131d792bc442dc648959de271cde9c
davianpup <davianp651@gmail.com>
2025-09-23 19:27:46 -0300

    Add files via upload

---
+++ Atividade05DaviAngeloJava.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade05.davi.angelo.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade05DaviAngeloJava {
+
+    public static void main(String[] args) {
+        int n,ctd=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        n = input.nextInt();
+        
+        while(ctd <n){
+            System.out.print(ctd);
+            ctd++;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
