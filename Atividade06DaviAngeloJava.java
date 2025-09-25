fe5edf915eadaade28170665d697e3259b23cc83
davianpup <davianp651@gmail.com>
2025-09-25 18:52:07 -0300

    Add files via upload

---
+++ Atividade06DaviAngeloJava.java
@@ -0,0 +1,170 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade06.davi.angelo.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade06DaviAngeloJava {
+
+    public static void main(String[] args) {
+//        int nota;
+//        Scanner input = new Scanner(System.in);
+//        do{
+//            System.out.print("Dê uma nota entre 0 e 10: ");
+//            nota = input.nextInt();
+//            if(nota<0 && nota>10){
+//               System.out.print("Valor inválido! Tente novamente"); 
+//            }
+//        }while(nota<0 && nota>10);
+//        System.out.print("Valor válido!");
+
+        //Questão 2
+//        int n,soma=0,media,ctd=0;
+//        Scanner input = new Scanner(System.in);
+//        do{
+//            System.out.print("Digite um número(0 para sair): ");
+//            n = input.nextInt();
+//            soma += n;
+//            ctd++;
+//            media = soma/ctd;
+//            if(n==0){
+//                System.out.println("Soma total: "+soma);
+//                System.out.println("Quantidade: "+ctd);
+//                System.out.println("Média: "+media);
+//            }
+//        }while(n!=0);
+        
+        //Questão 3
+//        int n,ncert,ctd=0;
+//        Scanner input = new Scanner(System.in);
+//        ncert = ((int)(Math.random()*100)+1);
+//        do{
+//            System.out.println("Palpite: ");
+//            n = input.nextInt();
+//            ctd++;
+//            if(n<ncert){
+//                System.out.println("Mais alto!");
+//            }
+//            if(n>ncert){
+//                System.out.println("Mais baixo");
+//            }
+//            if(n==ncert){
+//                System.out.println("Tentativas: "+ctd);
+//                System.out.println("Número certo: "+ncert);
+//            }
+//        }while(n!=ncert);
+
+        //Questão 4
+//        Scanner input = new Scanner(System.in);
+//        int n;
+//        String op;
+//        do{
+//            System.out.print("Digite um número inteiro: ");
+//            n = input.nextInt();
+//            if(n>0){
+//                for(int i=0; i<n; i++){
+//                    System.out.print(n-i);
+//                }
+//                System.out.print("Digite S para continuar ou N para sair: ");
+//                op = input.nextLine();
+//            }
+//        }while(n<=0 || op=="S");
+
+        //Questão 5
+//        int ctd=3;
+//        String senha_c= "senai123",senha;
+//        boolean certa;
+//        Scanner input = new Scanner(System.in);
+//        do{
+//            System.out.print("Digite sua senha: ");
+//            senha = input.nextLine();
+//            if(senha.equals(senha_c)==true){
+//                System.out.print("Acesso Permitido!");
+//                break;
+//            }else{
+//                --ctd;
+//                System.out.println("Senha Incorreta. Tentativas restantes: "+ctd);
+//            }
+//        }while(ctd!=0);
+//        if(ctd==0){
+//            System.out.println("Conta Bloqueada!");
+//        }
+
+        //Questão 6
+//        int n,mult,res;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite um número: ");
+//        n = input.nextInt();
+//        for(int ctd=0; ctd<=10; ctd++){
+//            res = n*ctd;
+//            System.out.println(n+"x"+ctd+"="+res);
+//        }
+
+        //Questão 7
+//        int n,soma1=0,soma2=0;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Digite um número: ");
+//        n = input.nextInt();
+//        for(int ctd=0; ctd<=n;ctd++){
+//            if(ctd%2==0){
+//                soma1 +=ctd;
+//            }else{
+//                soma2 +=ctd;
+//            }
+//            if(ctd>=n){
+//                System.out.println("Soma pares: "+soma1);
+//                System.out.println("Soma ímpares: "+soma2);
+//            }
+//            
+//        }
+// 
+        //Questão 8
+//        Scanner input = new Scanner(System.in);
+//        int n;
+//        System.out.print("Digite um número maior que 1: ");
+//        n = input.nextInt();
+//        if(n<=1){
+//            System.out.print("Número inválido!");
+//        }else{
+//            for(int i=2; i<=n; i++){
+//                boolean primo=true;
+//                for(int k=2; k*k<=i;k++){
+//                    if(i%k==0){
+//                        primo=false;
+//                        break;
+//                    }
+//                }
+//                if(primo==true){
+//                    System.out.print(i+",");
+//                }
+//            }
+//        }
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
