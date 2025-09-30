d376472314796d9118228a9930eebdbe63d0406d
davianpup <davianp651@gmail.com>
2025-09-30 18:12:16 -0300

    Add files via upload

---
+++ Atividade07DaviAngeloJava.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+import java.util.Scanner;
+/** 
+ * 
+ * 
+ * 
+ *
+ * @author Aluno
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in); // AVERIGUAR
+//        int[][] a = new int[16];
+//        
+//        for(int ctd=0; ctd<a.length; ctd++){
+//            System.out.print("Digite o valor "+ctd+": ");
+//            a[ctd] = input.nextInt();
+//            if(ctd==a.length){
+//                System.out.print(a);
+//                break;
+//            }
+//        }
+//        System.out.print(a);
+
+        //Questão 2
+//        int[] a = new int[8];
+//        int maior=1;
+//        Scanner input = new Scanner(System.in);
+//        for(int ctd=0;ctd<a.length;ctd++){
+//            System.out.print("Digite o número posição"+ctd+": ");
+//            a[ctd] = input.nextInt();
+//            for(int i=0;i<a.length;i++){
+//                if(maior<a[i]){
+//                    maior = a[i];
+//                    System.out.print("Maior = "+maior);
+//                    break;
+//                }
+//            }
+//        }
+        
+        //Questão 3
+//        int[] vet1 = new int[5], vet2 = new int[5];
+//        Scanner input = new Scanner(System.in);
+//        for(int ctd=0;ctd<vet1.length;ctd++){
+//            System.out.print("Digite o valor posição "+ctd+": ");
+//            vet1[ctd] = input.nextInt();
+//            if(ctd>=vet1.length){
+//            vet2 = vet1;
+//            System.out.print("Primeiro vetor: "+vet1);
+//            System.out.print("Segundo vetor: "+vet2);
+//            }
+//        }
+
+        //Questão 4
+//        double[] a = new double[20];
+//        double soma=0,media=0;
+//        Scanner input = new Scanner(System.in);
+//        for(int ctd=0;ctd<a.length;ctd++){
+//            System.out.print("Digite o valor "+ctd+": ");
+//            a[ctd] = input.nextDouble();
+//                soma += a[ctd];
+//        }
+//        media = soma/a.length;
+//        System.out.print("Média: "+media);
+//        for(int j=0;j<a.length;j++){
+//            if(media>a[j]){
+//                System.out.println(a[j]);
+//                }
+//        }
+//        
+        //5
+     
+      // String senha;
+       
+       
+      // do{
+       //    System.out.print("digite uma senha  valida (minimo 8 digitos: ) ");
+        //   senha=entrada.nextLine();
+        //   if (senha.length()>8){
+        //   int faltam=8-senha.length();
+        //   System.out.print("a sua senha precisa de: "+faltam);}
+           
+     //  } while(senha.length()<8);
+     //  System.out.print("Senha Válida");
+       
+     //  if (senha.length()>8){
+      ///    int faltaam=8-senha.length();
+      // }
+       
+       
+      //6
+     
+     
+   
+       // System.out.print("Digite uma frase: ");
+     //  String frase = entrada.nextLine();
+
+      //  int vogais = 0, consoantes = 0, espacos = 0, outros = 0;
+//
+     //   for (int i = 0; i < frase.length(); i++) {
+      //      char ch = frase.charAt(i);
+//
+        //    if ("aeiouAEIOU".indexOf(ch) != -1) {
+          //      vogais++;
+         //   } else if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
+         //       consoantes++;
+         //   } else if (ch == ' ') {
+         //       espacos++;
+         //   } else {
+         //       outros++;
+         //   }
+      //  }
+
+     // /  System.out.println("Vogais: " + vogais);
+     ///   System.out.println("Consoantes: " + consoantes);
+     //   System.out.println("Espaços: " + espacos);
+       // System.out.println("Outros: " + outros);
+       
+       
+       //7
+       
+      // String texto;
+      //  int quantidade = 0;
+
+      //  System.out.print("Digite uma frase: ");
+      //  texto = entrada.nextLine();
+
+      //  int encontrar_palavra = texto.indexOf("java");
+
+       // while (encontrar_palavra >= 0) {
+       //     System.out.println("Encontrada a palavra na posição: " + encontrar_palavra);
+        //    quantidade++;
+//
+        //    encontrar_palavra = texto.indexOf("java", encontrar_palavra + 1);
+     //   }
+
+     //   if (quantidade == 0) {
+     //       System.out.println("Palavra não encontrada.");
+     //   } else {
+       //     System.out.println("Quantidade de ocorrências: " + quantidade);
+       // }
+
+       
+       
+       //8
+//    String nomeUsuario;
+//        String usuarioCorreto = "yurilindo";
+//        int tentativas = 3;
+//        boolean acessoConcedido = false;
+//
+//        do {
+//            System.out.print("Digite o nome de usuário: ");
+//            nomeUsuario = entrada.nextLine();
+//
+//            if (nomeUsuario.equals(usuarioCorreto)) {
+//                System.out.println("Usuário correto. Acesso concedido.");
+//                acessoConcedido = true;
+//                break;
+//            } else {
+//                tentativas--;
+//                if (tentativas > 0) {
+//                    System.out.println("Usuário incorreto. Tentativas restantes: " + tentativas);
+//                } else {
+//                    System.out.println("Usuário incorreto. Você excedeu o máximo de tentativas. Conta bloqueada.");
+//                }
+//            }
+//        } while (tentativas > 0);
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
